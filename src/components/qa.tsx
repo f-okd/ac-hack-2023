b063675f7a88d3faffa71ec865068d37c04d4aba
davididowu2001 <idowu.subomi9@gmail.com>
2023-11-18 22:04:48 +0000

    Saving progress so far

---
+++ src/components/qa.tsx
@@ -0,0 +1,21 @@
+// QA.tsx
+import React from 'react';
+
+const QA: React.FC = () => {
+    return (
+        <div>
+            <h2>Q&A</h2>
+            <div>
+                <h3>What prerequisites do I need?</h3>
+                <p>Basic understanding of...</p>
+            </div>
+            <div>
+                <h3>How long is the course?</h3>
+                <p>The course spans over...</p>
+            </div>
+            {/* Add more questions and answers here */}
+        </div>
+    );
+};
+
+export default QA;
