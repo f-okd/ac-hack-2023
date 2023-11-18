024e89e303933ba7fd4769bc58a33c5b3d5c4056
Michael Ly <michaelly60@outlook.com>
2023-11-18 17:09:56 +0000

    basic template and module card

---
+++ src/components/modulecards.tsx
@@ -0,0 +1,15 @@
+export default function ModuleCard() {
+  return (
+    <main className="container">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/pages/mycourses.tsx
@@ -0,0 +1,27 @@
+import ModuleCard from "@/components/modulecards";
+
+export default function Home() {
+  return (
+    <main className="container">
+      <div className="flex w-full flex-col">
+        <div className="card flex grid h-40 items-center rounded-box bg-base-300">
+          <p className="ml-40 text-left">My Learning</p>
+        </div>
+      </div>
+      <div className="">
+        <div role="tablist" className="ml-40 mr-40 tabs tabs-bordered">
+          <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="My Courses"/>
+          <div role="tabpanel" className="tab-content">
+            <ModuleCard />
+          </div>
+
+          <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Completed"/>
+            <div role="tabpanel" className="tab-content">
+
+            </div>
+
+        </div>
+      </div>
+    </main>
+  );
+}
