e7473c77d796bd715760adb39063b8d099ba7cf9
davididowu2001 <idowu.subomi9@gmail.com>
2023-11-18 22:35:25 +0000

    added video

---
+++ src/components/videoPlayer.tsx
@@ -0,0 +1,27 @@
+// VideoPlayer.tsx
+import React from 'react';
+
+interface VideoPlayerProps {
+    src: string; // YouTube video URL
+}
+
+const VideoPlayer: React.FC<VideoPlayerProps> = ({ src }) => {
+    // Extract the video ID from the YouTube URL
+    const videoId = src.split('v=')[1].split('&')[0];
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <iframe
+                width="560" 
+                height="300" 
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="Learn CSS" 
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen>
+            </iframe>
+        </div>
+    );
+};
+
+export default VideoPlayer;
--- src/pages/coursepage.tsx
+++ src/pages/coursepage.tsx
@@ -4,6 +4,7 @@ import {CourseDrawer} from '../components/courseDrawer';
 import QA from '../components/qa';
 import Overview from '../components/overView';
 import CourseNavbar from '../components/courseNavbar';
+import VideoPlayer from '../components/videoPlayer';
 
 type TabName = 'overview' | 'QA';
 
@@ -14,12 +15,13 @@ export default function CoursePage() {
         <div className='course-page'>
             <CourseDrawer />
             <div className='course-content'>
-                <CourseNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
+            </div>
+            <VideoPlayer src="https://www.youtube.com/watch?v=7G7hkaMfo9g" />
+            <CourseNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
                 <div className='tab-content'>
                     {activeTab === 'overview' && <Overview />}
-                    {activeTab === 'QA' && <QA />}
+                    {activeTab === 'QA' && <QA/>}
                 </div>
-            </div>
         </div>
     );
 }
