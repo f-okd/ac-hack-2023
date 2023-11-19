ad57c4d500b9287bf19849aefd1dfafca6286b0d
Michael Ly <michaelly60@outlook.com>
2023-11-19 07:04:00 +0000

    updated course page description

--- src/components/courseNavbar.tsx
+++ src/components/courseNavbar.tsx
@@ -16,7 +16,7 @@ export default function CourseNavbar({ activeTab, setActiveTab }: CourseNavbarPr
                 <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Overview" defaultChecked={true}/>
                 <div role="tabpanel" className="tab-content p-10">
                    <h1 className="font-bold text-2xl">About this Course</h1>
-                   <p> Java is one of the most widely used and versatile programming languages in the world. This comprehensive introductory course is designed to equip students with the fundamental skills and knowledge needed to become proficient Java programmers. Whether you're a complete beginner or have some programming experience, this course will guide you through the core concepts and practical applications of Java.
+                   <p> Learning web development not only equips individuals with the skills to create dynamic and visually appealing websites but also opens doors to a multitude of opportunities in the ever-evolving tech industry. This comprehensive introductory course is designed to equip students with the fundamental skills and knowledge needed to become proficient web development programmers. Whether you're a complete beginner or have some programming experience, this course will guide you through the core concepts and practical applications of web development.
                    </p>
                 </div>
                 <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Comments" />
