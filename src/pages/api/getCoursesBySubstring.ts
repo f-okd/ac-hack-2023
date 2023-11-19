5bc350c37e8468641877398d26c45340b8d37bb5
A-2-4-8-5-10-9-7-3-6-1 <olaosindero@gmail.com>
2023-11-19 01:04:41 +0000

    modified:   src/pages/api/getCoursesBySubstring.ts 	modified:   src/pages/searchresults.tsx

--- src/pages/api/getCoursesBySubstring.ts
+++ src/pages/api/getCoursesBySubstring.ts
@@ -4,7 +4,7 @@ import {  Course, Database } from "../../types/model_interfaces";
 import { promises } from 'fs';
 
 type Data = {
-  course?: Course,
+  courses?: Course[],
   message?: string,
 }
 
@@ -12,16 +12,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  if (req.method == "GET") {
-    const file = await promises.readFile(process.cwd() + "/public/database.json", "utf8");
-    const db: Database = JSON.parse(file);
-    const { substring } = req.body;
-    const course = db.courses.find(course => course.title.includes(substring));
+  const file = await promises.readFile(process.cwd() + "/public/database.json", "utf8");
+  const db: Database = JSON.parse(file);
+  const { substring } = req.body;
+  const courses = db.courses.filter(course => course.title.toLowerCase().includes(substring));
+  console.log(substring);
+  console.log(courses);
+  console.log(db.courses);
 
-    if (!course) {
-      res.status(200).json({ message: "no matching courses." });
-    } else {
-      res.status(200).json({ course });
-    }
+  if (!courses) {
+    res.status(200).json({ message: "no matching courses." });
+  } else {
+    res.status(200).json({ courses });
   }
 }
+6
--- src/pages/searchresults.tsx
+++ src/pages/searchresults.tsx
@@ -2,26 +2,30 @@ import { useEffect, useState } from "react";
 import { Course } from "../types/model_interfaces";
 import React from "react";
 
-export default function SearchResults(query: string="I") {
+export default function SearchResults({ query="I" }:{ query: string }) {
     const [courses, setCourses] = useState<Course[]>();
     const [loading, setLoading] = useState<Boolean>(true);
     
     useEffect(() => {
-        const url = new URL("http://localhost:3000/api/getCoursesBySubstring/");
-
-        url.searchParams.append("substring", query);
-        fetch(url)
+        fetch("./api/getCoursesBySubstring/", {
+            body: JSON.stringify({
+                substring: query.toLowerCase()
+            }),
+            method: 'POST',
+        })
         .then(async res => await res.json())
         .then(data => {
-            setCourses(data);
+            setCourses(data.courses);
             setLoading(false)
         });
-    });
+    }, [query]);
 
     if (loading) {
         return <><p>xqoppo</p></>
     }
 
-    return <>{courses}</>
+    console.log(courses);
+
+    return <>{courses?.map(course => <p>{course.title}</p>)}</>
 
 }
